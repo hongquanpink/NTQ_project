a135467a64c10644ac81502ccc38fba0de9fbc88
quan191200846@lms.utc.edu.vn <quan191200846@lms.utc.edu.vn>
2023-02-27 10:47:57 +0700

    second commit

--- NTQ_Solution_web/DAL(Data Access Layer)/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DAL_Data_Access_Layer_.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ NTQ_project/DAL(Data Access Layer)/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DAL_Data_Access_Layer_
+{
+    public class Class1
+    {
+
+    }
+}
